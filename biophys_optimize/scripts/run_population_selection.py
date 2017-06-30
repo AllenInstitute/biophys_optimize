3d406f9f9539af10a66aebede590a5f5369e1bda
Nathan Gouwens <nathang@alleninstitute.org>
2017-06-30 14:21:11 -0700

    Adding missing selection stage after first optimization

---
+++ biophys_optimize/scripts/run_population_selection.py
@@ -0,0 +1,46 @@
+import argparse
+import allensdk.core.json_utilities as ju
+
+import biophys_optimize.population_selection as ps
+
+import json_module as jm
+import marshmallow as mm
+
+
+class ModelFit(mm.fields.Dict):
+    fit_type = mm.fields.Str(description="")
+    hof_fit = jm.InputFile(description="")
+    hof = jm.InputFile(description="")
+
+
+class PopulationSelectionPaths(mm.Schema):
+    fits = mm.fields.List(ModelFit, description="")
+
+
+class PopulationSelectionParameters(jm.ModuleParameters):
+    paths = mm.fields.Nested(PopulationSelectionPaths)
+
+
+class PopulationSelectionModule(jm.JsonModule):
+    def __init__(self, *args, **kwargs):
+        super(PopulationSelectionModule, self).__init__(schema_type=PopulationSelectionParameters,
+                                                        *args, **kwargs)
+
+
+def main():
+    module = PopulationSelectionModule()
+
+    fits = module.args["paths"]["fits"]
+    populations = ps.population_info(fits)
+    starting_populations = ps.select_starting_population(populations)
+
+    output = {
+        "paths": {
+            "starting_populations": starting_populations,
+        }
+    }
+
+    ju.write(module.args["output_json"], output)
+
+
+if __name__ == "__main__": main()
