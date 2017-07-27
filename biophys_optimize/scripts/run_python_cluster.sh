1dbcbf5b9a32f9714acd8d4841b1716182964e68
David Feng <davidf@alleninstitute.org>
2017-07-26 21:43:10 -0700

    cluster script rd 2

---
+++ biophys_optimize/scripts/run_python_cluster.sh
@@ -0,0 +1,12 @@
+export ALLENSDK_PATH=/data/informatics/mousecelltypes/biophys_optimize/allensdk/
+export ARGSCHEMA_PATH=/data/informatics/mousecelltypes/biophys_optimize/argschema/
+export MM_PATH=/data/informatics/mousecelltypes/biophys_optimize/marshmallow/
+
+NEURON_HOME=/shared/utils.x86_64/nrn-7.4-1370
+export PYTHONPATH=${NEURON_HOME}/lib/python:${ALLENSDK_PATH}:${ARGSCHEMA_PATH}:${MM_PATH}:${PYTHONPATH}
+export PYTHON_HOME=/shared/utils.x86_64/python-2.7
+export PYTHON=${PYTHON_HOME}/bin/python
+export PATH=${PYTHON_HOME}/bin:${NEURON_HOME}/x86_64/bin:${PATH}
+NUM_PROCS=240
+
+mpiexec -np $NUM_PROCS $@
