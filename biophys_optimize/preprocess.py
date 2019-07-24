d27b8936662533273c467fb97d492a098c9a2d82
Nathan Gouwens <nathang@alleninstitute.org>
2019-07-24 15:26:52 -0700

    starting to refactor to use ipfx

--- biophys_optimize/preprocess.py
+++ biophys_optimize/preprocess.py
@@ -3,22 +3,23 @@
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
-import subprocess
 import re
 import os
 import sys
 import logging
-import pkg_resources
-from collections import Counter
+import yaml
 
 from allensdk.config.manifest import Manifest
 
-from biophys_optimize import check_fi_shift
-from biophys_optimize import sweep_functions as sf
+import .check_fi_shift as check_fi_shift
+import .sweep_functions as sf
+
 
 DEFAULT_SEEDS = [1234, 1001, 4321, 1024, 2048]
 
+
 class NoUsableSweepsException(Exception): pass
+
 
 class FitStyle():
     F6 = "f6"
@@ -65,6 +66,7 @@ class FitStyle():
     def map_stage_2(stage_1):
         return  FitStyle.FIT_STAGE_MAP[stage_1]
 
+
 def get_cap_check_indices(i):
     """Find the indices of the upward and downward pulses in the current trace `i`
 
@@ -77,87 +79,103 @@ def get_cap_check_indices(i):
     return up_idx[2::2], down_idx[1::2]
 
 
-def find_core1_trace(data_set, c1_sweeps):
+def select_core1_trace(sweep_set, start, end,
+        exceed_rheobase=39.0, min_isi_cv=0.3, fraction_isi_cv=0.2):
     """Identify a Core 1 long square sweep as the optimization target"""
-    ext = sf.sweep_set_extractor_from_list(c1_sweeps, data_set,
-                                           sf.C1LS_START, sf.C1LS_END)
-    ext.process_spikes()
+    step_analysis = StepAnalysis(start, end)
+    step_analysis.analyze(sweep_set)
+    sweep_info = step_analysis.sweep_features()
+    quality_values = [is_sweep_good_quality(sd["threshold_t"].values, start, end)
+                      for sd in step_analysis.spikes_data()]
+    sweep_info["quality"] = quality_values
 
-    sweep_info = []
-    for sweep_num, swp in zip(c1_sweeps, ext.sweeps()):
-        sweep_info.append({"amp": swp.sweep_feature("stim_amp"),
-                           "rate": swp.sweep_feature("avg_rate"),
-                           "quality": is_trace_good_quality(swp),
-                           "isi_cv": swp.sweep_feature("isi_cv") if swp.sweep_feature("avg_rate") > 0 else np.nan,
-                           "sweep_num": sweep_num})
+    spiking_features = step_analysis.suprathreshold_sweep_features()
+    spiking_amps = spiking_features[spiking_features["stim_amp"] > 0].sort_values("stim_amp")
+    if spiking_amps.empty:
+        raise RuntimeError("Cannot find rheobase sweep")
+    rheobase_amp = spiking_amps.iloc[0]
+    unique_amps = np.unique(np.rint(spiking_amps.values))
 
-    rheobase_amp = 1e12
-    for s in sweep_info:
-        if s["amp"] < rheobase_amp and s["rate"] > 0:
-            rheobase_amp = s["amp"]
-    sweep_to_use_amp = 1e12
-    sweep_to_use_isi_cv = 1e12
-    sweep_to_use = -1
-    for s in sweep_info:
-        if s["quality"] and s["amp"] >= 39.0 + rheobase_amp and s["isi_cv"] < 1.2 * sweep_to_use_isi_cv:
-            use_new_sweep = False
-            if sweep_to_use_isi_cv > 0.3 and ((sweep_to_use_isi_cv - s["isi_cv"]) / sweep_to_use_isi_cv) >= 0.2:
-                use_new_sweep = True
-            elif s["amp"] < sweep_to_use_amp:
-                use_new_sweep = True
-            if use_new_sweep:
-                logging.info("now using sweep %d", s["sweep_num"])
-                sweep_to_use = s["sweep_num"]
-                sweep_to_use_amp = s["amp"]
-                sweep_to_use_isi_cv = s["isi_cv"]
-    if sweep_to_use == -1:
+    sweep_to_use_mean_isi_cv = 1e12
+    sweeps_to_use = []
+    for a in unique_amps:
+        if a < exceed_rheobase + rheobase_amp:
+            continue
+        mask = np.rint(sweep_info["stim_amp"].values) == a
+        quality = sweep_info.loc[mask, "quality"].values
+        if np.sum(quality) == 0:
+            continue
+        isi_cv = sweep_info.loc[mask, "isi_cv"].values[quality]
+        if ((sweep_to_use_mean_isi_cv >= min_isi_cv) and
+            np.all(isi_cv < (1. - fraction_isi_cv) * sweep_to_use_mean_isi_cv)):
+            sweeps_to_use = np.array(sweep_set.sweeps)[mask][quality].tolist()
+            sweep_to_use_mean_isi_cv = isi_cv.mean()
+
+    if len(sweep_to_use) == 0:
         logging.info("Could not find appropriate core 1 sweep!")
-        return np.array([])
+        return []
     else:
-        return np.array([sweep_to_use])
+        return sweeps_to_use
 
 
-def is_trace_good_quality(swp):
+def is_sweep_good_quality(spike_times, start, end, min_rate=5):
     """Check whether the sweep passes several quality criteria:
-    - Exceeds minimum rate of 5 spikes/sec
+    - Matches or exceeds defined minimum rate
     - Is not transient, defined as the time from the last spike to the
       end of the stimulus being twice as long as the average of the last two
       interspike intervals
-    - Does not have a pause (defined by check_for_pause() below)
+    - Does not have a pause (defined by has_pause())
 
-    Parameter:
-    swp: EphysSweepFeatureExtractor
-        Trace to check
+    Parameters
+    ----------
+    spike_times: array
+
+    end: float
+        End time of stimulus interval
+
+    min_rate: float
+        Minimum firing rate to consider (spikes/sec)
+
+    Returns
+    -------
+    good_quality: bool
+        Whether sweep should be considered for fitting
     """
-    spike_times = swp.spike_feature("threshold_t")
-    rate = swp.sweep_feature("avg_rate")
+    rate = np.sum((spike_times > start) & (spike_times < end)) / (end - start)
 
-    min_rate = 5.
     if rate < min_rate:
         return False
 
-    time_to_end = swp.end - spike_times[-1]
+    if len(spike_times) < 3:
+        logging.debug("Need at least three spikes to determine sweep quality")
+        return False
+
+    time_to_end = end - spike_times[-1]
     avg_end_isi = ((spike_times[-1] - spike_times[-2]) + (spike_times[-2] - spike_times[-3])) / 2.
 
     if time_to_end > 2. * avg_end_isi:
         return False
 
     isis = np.diff(spike_times)
-    if check_for_pause(isis):
+    if has_pause(isis):
         return False
 
     return True
 
 
-def check_for_pause(isis):
-    """Check whether the sweep has a pause (roughly defined)
+def has_pause(isis):
+    """Check whether a set of ISIs has a pause
 
     A pause here is an interspike interval (ISI) that is at least three times
     longer than its adjacent ISIs.
 
-    Parameter:
-    swp: EphysSweepFeatureExtractor
-        Trace to check
+    Parameters
+    ----------
+    isis: array
+
+    Returns
+    -------
+    Whether set of ISIs contains a pause
     """
     if len(isis) <= 2:
         return False
@@ -168,7 +186,7 @@ def check_for_pause(isis):
     return False
 
 
-def target_features(ext):
+def target_features(sweep_data, spike_data_list, min_std_dict=None):
     """Determine target features from sweeps in set extractor"""
     ext.process_spikes()
     for swp in ext.sweeps():
@@ -184,26 +202,10 @@ def target_features(ext):
     sweep_keys = swp.sweep_feature_keys()
     spike_keys = swp.spike_feature_keys()
 
-    min_std_dict = {
-        'avg_rate': 0.5,
-        'adapt': 0.001,
-        'peak_v': 2.0,
-        'trough_v': 2.0,
-        'fast_trough_v': 2.0,
-        'slow_trough_delta_v': 0.5,
-        'slow_trough_norm_t': 0.05,
-        'latency': 5.0 * 1e-3, # seconds
-        'isi_cv': 0.1,
-        'mean_isi': 0.5 * 1e-3, # seconds
-        'first_isi': 1.0 * 1e-3, # seconds
-        'v_baseline': 2.0,
-        'width': 0.1 * 1e-3, # seconds
-        'upstroke': 5.0,
-        'downstroke': 5.0,
-        'upstroke_v': 2.0,
-        'downstroke_v': 2.0,
-        'threshold_v': 2.0,
-    }
+    if min_std_dict is None:
+        # Load from default configuration file if not supplied
+        with open(os.path.join(os.path.dirname(__file__), '../config/minimum_feature_standard_deviations.yaml')) as f:
+            min_std_dict = yaml.load(f)
 
     target_features = []
     for k in min_std_dict:
@@ -223,60 +225,93 @@ def target_features(ext):
     return target_features
 
 
-def select_sweeps(sweeps_input, data_set):
+def select_core_1_or_core_2_sweeps(
+        core_1_lsq, core_1_start, core_1_end,
+        core_2_lsq, core_2_start, core_2_end,
+        fi_shift_threshold=30.0):
     """Identify the sweep or sweeps to use as targets for optimization
 
+    Prefers Core 2 sweeps because they are longer and usually have repeats.
     Selects a Core 1 sweep if the Core 2 sweeps do not exist or if the
-    f-I curve has shifted by at least 30 pA.
+    f-I curve has shifted by at least 30 pA (since Core 1 occurs earlier in the
+    overall experimental protocol)
 
     Parameters
     ----------
-    sweeps_input: dict
-        Dictionary with relevant sweep numbers in `core_1_long_squares` and
-        core_2_long_squares` keys
-    data_set: NwbDataSet
-        container of sweep data
+    core_1_lsq: SweepSet
+        "Core 1" long-square sweeps (1 second long stimulus, no repeats expected)
+    core_1_start: float
+        Start time of stimulus interval for Core 1 sweeps
+    core_1_end: float
+        End time of stimulus interval for Core 1 sweeps
+    core_2_lsq: SweepSet
+        "Core 2" long-square sweeps (2 seconds long stimulus, repeats expected)
+    core_2_start: float
+        Start time of stimulus interval for Core 2 sweeps
+    core_2_end: float
+        End time of stimulus interval for Core 2 sweeps
+    fi_shift_threshold: float (optional, default 30.0)
+        Maximum allowed f-I curve shift to still select Core 2 sweeps
 
     Returns
     -------
-    sweeps_to_fit: list
-        sweep numbers of sweeps for optimization target
+    sweeps_to_fit: SweepSet
+
     start: float
         time of stimulus start (in seconds)
     end: float
         time of stimulus end (in seconds)
     """
-    fi_shift, n_core2 = check_fi_shift.estimate_fi_shift(sweeps_input, data_set)
-    fi_shift_threshold = 30.0
-    if abs(fi_shift) > fi_shift_threshold:
-        logging.info("f-I curve shifted; using Core 1")
-        sweeps_to_fit = find_core1_trace(data_set, sweeps_input["core_1_long_squares"])
-        start, end = sf.C1LS_START, sf.C1LS_END
-    else:
-        c2_ext = sf.sweep_set_extractor_from_list(sweeps_input["core_2_long_squares"],
-                                                  data_set, sf.C2LS_START, sf.C2LS_END)
-        c2_ext.process_spikes()
-        core2_amp_counter = Counter(c2_ext.sweep_features("stim_amp"))
-        common_amps = [a[0] for a in core2_amp_counter.most_common(3)]
 
-        n_good = {a: 0 for a in common_amps}
-        sweeps_by_amp = {a: [] for a in common_amps}
-        for i, swp in enumerate(c2_ext.sweeps()):
-            amp = swp.sweep_feature("stim_amp")
-            if is_trace_good_quality(swp):
-                n_good[amp] += 1
-                sweeps_by_amp[amp].append(i)
+    use_core_1 = False
+    if len(core_2_lsq.sweeps) == 0:
+        logging.info("No Core 2 sweeps available")
+        use_core_1 = True
+    else:
+        fi_shift = check_fi_shift.estimate_fi_shift(
+            core_1_lsq, core_1_start, core_1_end,
+            core_2_lsq, core_2_start, core_2_end)
+        if abs(fi_shift) > fi_shift_threshold:
+            logging.info(
+                "f-I curve shifted by {} (exceeding +/- {}); "
+                "using Core 1".format(fi_shift, fi_shift_threshold))
+            use_core_1 = True
+
+    if not use_core_1:
+        # Try to find Core 2 sweeps that work
+        core2_analysis = StepAnalysis(core_2_start, core_2_end)
+        core2_analysis.analyze(core_2_lsq)
+        core2_sweep_features = core2_analysis.sweep_features()
+        stim_amps = np.rint(core2_sweep_features["stim_amp"].values)
+
+        n_good = {}
+        sweeps_by_amp = {}
+        for amp, swp, spike_data in zip(stim_amps, core_2_lsq.sweeps,
+                core2_analysis.spikes_data()):
+            spike_times = spike_data["threshold_t"].values
+            if is_sweep_good_quality(spike_times, core_2_start, core_2_end):
+                if amp in n_good:
+                    n_good[amp] += 1
+                    sweeps_by_amp[amp].append(swp)
+                else:
+                    n_good[amp] = 1
+                    sweeps_by_amp[amp] = [swp]
 
         if len(n_good) == 0 or max(n_good.values()) <= 1:
             logging.info("Not enough good Core 2 traces; using Core 1")
-            sweeps_to_fit = find_core1_trace(data_set, sweeps_input["core_1_long_squares"])
-            start, end = sf.C1LS_START, sf.C1LS_END
-        else:
-            best_amp = max(n_good, key=(lambda key: n_good[key]))
-            sweeps_to_fit = np.array(sweeps_input["core_2_long_squares"])[sweeps_by_amp[best_amp]]
-            start, end = sf.C2LS_START, sf.C2LS_END
+            use_core_1 = True
 
-    return sweeps_to_fit, start, end
+    if use_core_1:
+        sweeps_to_fit = select_core1_trace(core_1_lsq, core_1_start, core_1_end)
+        start = core_1_start
+        end = core_1_end
+    else:
+        best_amp = max(n_good, key=(lambda key: n_good[key]))
+        sweeps_to_fit = sweeps_by_amp[best_amp]
+        start = core_2_start
+        end = core_2_end
+
+    return SweepSet(sweeps_to_fit), start, end
 
 
 def prepare_for_passive_fit(sweeps, bridge_avg, is_spiny, data_set,
@@ -466,19 +501,33 @@ def max_i_for_depol_block_check(sweeps_input, data_set):
 def preprocess(data_set, swc_data, dendrite_type_tag,
                sweeps, bridge_avg, storage_directory,
                seeds=DEFAULT_SEEDS, jxn=-14.0):
+    """ Perform preprocessing tasks prior to passive fitting and optimization
 
-    is_spiny = True
+    Parameters
+    ----------
+    data_set: AibsDataSet
+    swc_data: dataframe
+    dendrite_type_tag: str
+    sweeps: dict
+    bridge_avg: float
+    storage_directory: str
+    seeds: list
+    jxn: float (optional, default -14.0)
+
+    Returns
+    -------
+
+    """
+
+    # TODO - Change this from a long string check
     if dendrite_type_tag == "dendrite type - aspiny":
         is_spiny = False
-        logging.debug("is spiny")
-    else:
         logging.debug("is not spiny")
+    else:
+        is_spiny = True
+        logging.debug("is spiny or sparsely spiny")
 
     # Check for fi curve shift to decide to use core1 or core2
-    sweeps_to_fit, start, end = select_sweeps(sweeps, data_set)
-    if len(sweeps_to_fit) == 0:
-        logging.info("No usable sweeps found")
-        raise NoUsableSweepsException("No usable sweeps found")
 
     paths, passive_info = prepare_for_passive_fit(sweeps["cap_checks"],
                                                   bridge_avg,
--- biophys_optimize/scripts/run_preprocessing.py
+++ biophys_optimize/scripts/run_preprocessing.py
@@ -3,15 +3,16 @@ import pandas as pd
 import os
 import argschema as ags
 
-from allensdk.core.nwb_data_set import NwbDataSet
 import allensdk.core.json_utilities as ju
+from ipfx.nwb_reader import nwb_reader
+import biophys_optimize.preprocess as preprocess
+from biophys_optimize.sweep_functions import sweeps_from_nwb
 
-from biophys_optimize.preprocess import preprocess, NoUsableSweepsException
 
 class PreprocessorPaths(ags.schemas.DefaultSchema):
-    nwb = ags.fields.InputFile(description="path to input NWB file")
-    swc = ags.fields.InputFile(description="path to input SWC file")
-    storage_directory = ags.fields.InputDir(description="path to storage directory")
+    nwb = ags.fields.InputFile(description="path to input NWB (Neurodata Without Borders) file")
+    swc = ags.fields.InputFile(description="path to input SWC (morphology) file")
+    storage_directory = ags.fields.OutputDir(description="path to storage directory")
 
 class PreprocessorSweeps(ags.schemas.DefaultSchema):
     core_1_long_squares = ags.fields.List(ags.fields.Int, description="list of core 1 long square sweep numbers")
@@ -25,30 +26,47 @@ class PreprocessorParameters(ags.ArgSchema):
     dendrite_type_tag = ags.fields.Str(description="dendrite type tag")
     sweeps = ags.fields.Nested(PreprocessorSweeps)
     bridge_avg = ags.fields.Float(description="average bridge balance")
-    
-def main():
+
+
+
+def main(paths, sweeps, dendrite_type, bridge_avg, output_json, **kwargs):
     """Main sequence of pre-processing and passive fitting"""
 
-    # This argschema package reads arguments from a JSON file
-    module = ags.ArgSchemaParser(schema_type=PreprocessorParameters, logger_name=None)
+    # Extract Sweep objects (from IPFX package) from NWB file
+    nwb_path = paths["nwb"] # nwb - neurodata without borders (ephys data)
+    nwb_data = nwb_reader(nwb_path)
+    core_1_lsq, c1_start, c1_end = sweeps_from_nwb(
+        nwb_data, sweeps["core_1_long_squares"])
+    core_2_lsq, c2_start, c2_end = sweeps_from_nwb(
+        nwb_data, sweeps["core_2_long_squares"])
 
-    nwb_path = module.args["paths"]["nwb"] # nwb - neurodata without borders (ephys data)
-    swc_path = module.args["paths"]["swc"] # swc - morphology data
-    storage_directory = module.args["paths"]["storage_directory"]
+    # Choose sweeps to train the model
+    sweeps_to_fit, start, end = preprocess.select_core_1_or_core_2_sweeps(
+        core_1_lsq, c1_start, c1_end,
+        core_2_lsq, c2_start, c2_end)
+    if len(sweeps_to_fit) == 0:
+        ju.write(output_json, { 'error': "No usable sweeps found" })
+        return
 
-    
-    
+    # Calculate the target features from the training sweeps
+
+
+    # Prepare for the passive fitting step
+
+
+    swc_path = paths["swc"] # swc - morphology data
+    storage_directory = paths["storage_directory"]
+
     try:
         paths, results, passive_info, s1_tasks, s2_tasks = \
-            preprocess(data_set=NwbDataSet(nwb_path),
+            preprocess(data_set=,
                        swc_data=pd.read_table(swc_path, sep='\s+', comment='#', header=None),
                        dendrite_type_tag=module.args["dendrite_type_tag"],
                        sweeps=module.args["sweeps"],
                        bridge_avg=module.args["bridge_avg"],
                        storage_directory=storage_directory)
     except NoUsableSweepsException as e:
-        ju.write(module.args["output_json"], { 'error': e.message })
-        return
+
 
 
     preprocess_results_path = os.path.join(storage_directory, "preprocess_results.json")
@@ -73,4 +91,7 @@ def main():
 
     ju.write(module.args["output_json"], output)
 
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    # argschema reads arguments from a JSON file or command line arguments
+    module = ags.ArgSchemaParser(schema_type=PreprocessorParameters, logger_name=None)
+    main(**module.args)
