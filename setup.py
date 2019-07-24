166cbda424995985977a2340e28d9179aaedd6f7
Nathan Gouwens <nathang@alleninstitute.org>
2019-07-24 15:24:30 -0700

    separate requirements file

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
 import biophys_optimize
 
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
 setup(
     version = biophys_optimize.__version__,
     name = 'biophys_optimize',
@@ -8,12 +11,12 @@ setup(
     author_email = 'nathang@alleninstitute.org',
     packages = find_packages(),
     package_data = {
-        'biophys_optimize': [ 'passive/*', 
+        'biophys_optimize': [ 'passive/*',
                               'fit_styles/*.json',
                               'modfiles/*.mod',
                               '*.hoc' ],
-        
+
         },
     include_package_data = True,
-    install_requires=['deap', 'neuron', 'numpy', 'allensdk', 'argschema' ]
+    install_requires=required
 )
